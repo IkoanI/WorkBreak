d8ec2e8829c05fb93999e06eba7afcfb2692393e
koan <koan329@gmail.com>
2025-04-07 21:30:31 -0400

    Added basic signup, login and logout functionality

--- backend/home/views.py
+++ backend/home/views.py
@@ -3,5 +3,4 @@ from django_nextjs.render import render_nextjs_page
 # Create your views here.
 async def home(request):
     # Your custom logic
-    #testing branch protection
     return await render_nextjs_page(request)
--- backend/workbreak/settings.py
+++ backend/workbreak/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_nextjs.apps.DjangoNextJSConfig',
-    'workbreak'
+    'workbreak',
+    'accounts',
+    'home'
 ]
 
 MIDDLEWARE = [
