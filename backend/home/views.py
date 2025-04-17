f9ae2cf151729614c9dfda361b5107a1aff370f8
Daksh Parekh <dgparekh2012@gmail.com>
2025-04-17 16:28:38 -0400

    Update views.py

--- backend/home/views.py
+++ backend/home/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django_nextjs.render import render_nextjs_page
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Visit
+from .serializers import VisitSerializer
+
+# Existing async view
 async def home(request):
     # Your custom logic
     return await render_nextjs_page(request)
+
+# New API view for user visits
+@api_view(['GET'])
+def user_visits(request):
+    user = request.user
+    visits = Visit.objects.filter(user=user)
+    serializer = VisitSerializer(visits, many=True)
+    return Response(serializer.data)
