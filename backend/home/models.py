01290d94ec3c8a3802d873b5e41e51853acf1aa0
alinalin10 <acl84408@uga.edu>
2025-04-19 17:06:51 -0400

    user reviews

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    restaurant_name = models.CharField(max_length=255)
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])  # 1 to 5 stars
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.restaurant_name} ({self.rating}★)"
+
