d9b761601f531622ac2e5788f2b6996c99f3dadd
koan <koan329@gmail.com>
2025-04-29 02:47:28 -0400

    fixing tripadvisor api call

--- backend/restaurants/urls.py
+++ backend/restaurants/urls.py
@@ -3,13 +3,13 @@ from django_nextjs.views import nextjs_page
 from . import views
 
 urlpatterns = [
-    path("<slug:slug>/", nextjs_page(), name="restaurants.page"),
-    path("discover/", nextjs_page(), name="restaurants.discover"),
     path("api/edit_review/", views.edit_review, name="restaurants.api.edit_review"),
+    path("api/tripadvisor_reviews/<str:location_id>/", views.tripadvisor_reviews, name="restaurants.api.tripadvisor_reviews"),
+    path("api/tripadvisor_search/<str:name>/<str:lat>/<str:lng>/", views.tripadvisor_search, name="restaurants.api.tripadvisor_search"),
     path("api/<slug:slug>/", views.get_restaurant, name="restaurants.api.get_restaurant"),
     path("api/<slug:slug>/create_review/", views.create_review, name="restaurants.api.create_review"),
     path("api/<slug:slug>/<str:place_id>/reviews/", views.get_reviews, name="restaurants.api.get_reviews"),
     path("api/<slug:slug>/<str:place_id>/reply_review/<int:review_id>/", views.reply_review, name="restaurants.api.reply_review"),
-    path("api/tripadvisor_search/<str:name>/<str:lat>/<str:lng>/", views.tripadvisor_search, name="restaurants.api.tripadvisor_search"),
-    path("api/tripadvisor_reviews/<str:location_id>/", views.tripadvisor_reviews, name="restaurants.api.tripadvisor_reviews"),
+    path("discover/", nextjs_page(), name="restaurants.discover"),
+    path("<slug:slug>/", nextjs_page(), name="restaurants.page"),
 ]
